5447cc44cb55579705c401953ba4a751f94b9d8b
eduardo <edavilazevallos@gmail.com>
2026-08-02 02:30:41 +0200

    fix(api): Fix api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api-crj.eduardodavila.es/',
+    baseURL: 'https://api-crj.eduardodavila.es',
     withCredentials: true,
     xsrfCookieName: 'XSRF-TOKEN',
     xsrfHeaderName: 'X-XSRF-TOKEN',
