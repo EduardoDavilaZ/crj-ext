57f6774d793a7c2b7a2ca7db1073c6cbc87b4142
eduardo <edavilazevallos@gmail.com>
2026-08-18 23:16:45 +0200

    feat(memes): Add memes page

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api-crj.eduardodavila.es',
+    baseURL: 'http://localhost:8000',
+    // baseURL: 'https://api-crj.eduardodavila.es',
     withCredentials: true,
     xsrfCookieName: 'XSRF-TOKEN',
     xsrfHeaderName: 'X-XSRF-TOKEN',
