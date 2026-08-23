dda7005e3496cb54c756020008f3aad59c7115f5
eduardo <edavilazevallos@gmail.com>
2026-08-23 15:52:30 +0200

    remove(ih form): Remove instructions in infancia hospitalizada form

--- src/features/projects/infancia-hospitalizada/Form.jsx
+++ src/features/projects/infancia-hospitalizada/Form.jsx
@@ -8,7 +8,6 @@ export default function Form() {
                 slug={config.slug}
                 projectTitle={config.projectTitle}
                 {...config.form}
-                instructions={<FormInstructions />}
             />
         </>
     );
